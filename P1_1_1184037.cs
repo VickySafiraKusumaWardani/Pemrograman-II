788cb92c966dfc77e2ea8365937fe16fb94eb60f
VickySafiraKusumaWardani <57054610+VickySafiraKusumaWardani@users.noreply.github.com>
2021-11-09 21:56:16 +0700

    Add files via upload

---
+++ P1_1_1184037.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Threading.Tasks;
+
+namespace P1_1_1184009
+{
+    class program
+    {
+        static void Main (string {} args)
+        {
+            console>WritLine("Hello Word From Visual Studio, I,m 1184037");
+        }
+    }
+}
